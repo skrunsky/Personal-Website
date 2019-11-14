352ebc6443f1d0c1a0fc6a7c3b6ddd116b7221bc
Sigurd Koldste <sigurd@ventora.dk>
2019-11-14 15:58:43 +0100

    Readded Sass + Started responsive design

--- js/script.js
+++ js/script.js
@@ -15,13 +15,18 @@ $(window).resize(function () {
 /******************************* 
  * FUNCTIONS
 *******************************/
+
+/**
+ * This function sets the hight of a div with the .profile class
+ * It will only do it, if the window is higher than it is wide
+ */
 function setHeight() {
     // Getting dynamic variables
     var footerHeight, profileInner, profileOuter, textHeight;
     footerHeight = $(".footer").outerHeight(true);
     profileInner = $(".profile").height();
     profileOuter = $(".profile").innerHeight();
-    textHeight = $(".profile__text").height();
+    textHeight = $(".profile__text").outerHeight();
 
     // Caculating the new height of the profile section
     var profilePadding, viewportHeight, newHeight;
@@ -32,5 +37,7 @@ function setHeight() {
     // Making sure narrow windows don't scale in the same way
     if(!(newHeight < textHeight)) {
         $(".profile__inner").height(newHeight);
+        $(".profile__image").height(newHeight);
+        console.log(newHeight);
     }
 }
